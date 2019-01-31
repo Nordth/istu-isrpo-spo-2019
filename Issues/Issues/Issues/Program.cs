51c15dd9708398ca49dd4ad149122dd1c66edf91
Arteeze <brbrbrbrfffff@yandex.ru>
2019-01-31 17:47:08 +0400

    Issues/Issues/Issues/Program.cs

--- Issues/Issues/Issues/Program.cs
+++ Issues/Issues/Issues/Program.cs
@@ -10,7 +10,7 @@ namespace Issues
     {
         public static List<int> OnlyUnique(List<int> sorted)
         {
-            for (int i = 0; i < sorted.LongCount() - 1; i++)
+            for (int i = 0; i <	 sorted.LongCount() - 1; i++)
             {
                 if (sorted.ElementAt(i) == sorted.ElementAt(i + 1))
                 {
@@ -19,6 +19,39 @@ namespace Issues
                 }
             }
             return sorted;
+        }
+		public static List<int> stroka()
+        {
+            List<int> sorted = new List<int>();
+            int n;
+            string s = Console.ReadLine();
+            while (s != "")
+            {
+                if (Int32.TryParse(s, out n) == true)
+                {
+                    sorted.Add(n);
+                    s = Console.ReadLine();
+                }
+            }
+            return sorted;
+        }
+        static void minmax(List<int> sorted)
+        {
+            int minnum = sorted[0];
+            int maxnum = sorted[0];
+            for (int i = 0; i <= sorted.Count - 1; i++)
+            {
+                if (sorted[i] < minnum)
+                {
+                    minnum = sorted.ElementAt(i);
+                }
+                if (sorted[i] > maxnum)
+                {
+                    maxnum = sorted.ElementAt(i);
+                }
+            }
+            Console.WriteLine("min = " + minnum);
+            Console.WriteLine("max = " + maxnum);
         }
 
         static void Main(string[] args)
