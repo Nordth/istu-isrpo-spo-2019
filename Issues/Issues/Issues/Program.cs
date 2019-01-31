e4d01bc2882f0504d3b3f21afc7f921f39fd97f7
D1sabler <sojiguy@mail.ru>
2019-01-31 15:15:38 +0400

    task3.3

--- Issues/Issues/Issues/Program.cs
+++ Issues/Issues/Issues/Program.cs
@@ -8,6 +8,19 @@ namespace Issues
 {
     class Program
     {
+        public static List<int> OnlyUnique(List<int> sorted)
+        {
+            for (int i = 0; i < sorted.LongCount() - 1; i++)
+            {
+                if (sorted.ElementAt(i) == sorted.ElementAt(i + 1))
+                {
+                    sorted.RemoveAt(i);
+                    i--;
+                }
+            }
+            return sorted;
+        }
+
         static void Main(string[] args)
         {
         }
